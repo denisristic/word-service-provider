8f738a499872b81785822f664779c52ebef02752
Denis Ristic <denis.ristic@perpetuum.hr>
2016-07-13 13:05:29 +0200

    FIXED WordServiceProvider

--- src/denisristic/WordServiceProvider/Provider/WordServiceProvider.php
+++ src/denisristic/WordServiceProvider/Provider/WordServiceProvider.php
@@ -2,15 +2,16 @@
 
 namespace denisristic\WordServiceProvider\Provider;
 
+use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 
 class WordServiceProvider implements ServiceProviderInterface
 {
     public function register(Container $app)
     {
-        $app['word'] = $app->share(function ($app) {
+        $app['word'] = function ($app) {
             return new \denisristic\WordServiceProvider\Generator\Word();
-        });
+        };
     }
 
 }
